2e585cb3638305b35df10c213324c26080ecbb5b
Илья Матвеев <matweev.ilja@yandex.ru>
2025-03-10 11:48:01 +0300

    added args to selector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -2,11 +2,14 @@ import { useSelector } from 'react-redux';
 // eslint-disable-next-line acid-plugin2/layers-import
 import { StateSchema } from '@/app/providers/StoreProvider/config/StateSchema';
 
-type Selector<T> = (state: StateSchema) => T;
-type Result<T> = [() => T, Selector<T>]
+type Selector<T, Args extends any[]> = (state: StateSchema, ...args: Args) => T;
+type Hook<T, Args extends any[]> = (...args: Args) => T
+type Result<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>]
 
-export function buildSelector<T>(selector: Selector<T>): Result<T> {
-    const useSelectorHook = () => useSelector(selector);
+export function buildSelector<T, Args extends any[]>(selector: Selector<T, Args>): Result<T, Args> {
+    const useSelectorHook: Hook<T, Args> = (
+        ...args: Args
+    ) => useSelector((state: StateSchema) => selector(state, ...args));
 
     return [useSelectorHook, selector];
 }
